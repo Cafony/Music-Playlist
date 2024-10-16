91f7b004cd8ffcf06a919bffc9104cc2bcb49c69
Toze Cerdeira <tozecerdeira@hotmail.com>
2024-10-16 12:04:18 +0100

    Add project files.

---
+++ Music Playlist/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Music_Playlist
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 131);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(432, 290);
+            this.listBox1.TabIndex = 0;
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonOpen.Location = new System.Drawing.Point(14, 442);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 1;
+            this.buttonOpen.Text = "Open";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonSave.Location = new System.Drawing.Point(176, 442);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 2;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonDelete.Location = new System.Drawing.Point(95, 441);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 3;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(268, 443);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(176, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Search";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(58, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(310, 29);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Playlist using DataSource";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(80, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(252, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Connects a List to winform ListBox";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label3.Location = new System.Drawing.Point(80, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(277, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "LOAD list on start - SAVE list on close";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 477);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.listBox1);
+            this.MinimumSize = new System.Drawing.Size(475, 500);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Playlist";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Music Playlist/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Music_Playlist
+{
+    public partial class Form1 : Form
+    {
+        List<string> mp3List = new List<string>();
+        BindingSource _bindingSource = new BindingSource();
+        string _playlistDefaultPath = System.IO.Path.Combine(Application.StartupPath, "playlist.txt");
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            _bindingSource.DataSource = mp3List; 
+            listBox1.DataSource = _bindingSource;
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            // Use FolderBrowserDialog to select a folder
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Get the selected folder path
+                    string selectedFolder = folderDialog.SelectedPath;
+
+                    // Get all .mp3 files in the selected folder
+                    string[] mp3Files = Directory.GetFiles(selectedFolder, "*.mp3");
+
+                    // Add each .mp3 file to the mp3List
+                    foreach (var file in mp3Files)
+                    {
+                        mp3List.Add(file);
+                    }
+
+                    // Refresh the binding to update listBox1
+                    //_bindingSource.ResetBindings(false);
+                    DisplayOnlyNames();
+                }
+            }
+
+        }
+
+        public void DisplayOnlyNames()
+        {
+            // Display only the file names in listBox1
+            List<string> fileNames = new List<string>();
+
+            foreach (var file in mp3List)
+            {
+                fileNames.Add(Path.GetFileName(file));  // Get only the file name
+            }
+
+            // Set the data source to the file names for display in listBox1
+            _bindingSource.DataSource = fileNames;
+
+            // Refresh the binding to update listBox1
+            _bindingSource.ResetBindings(false);
+        }
+
+
+        public void CreatePlaylistFile()
+        {
+            // Check if the file exists, if not, create an empty file
+            if (!System.IO.File.Exists(_playlistDefaultPath))
+            {
+                // Create an empty file
+                using (System.IO.FileStream fs = System.IO.File.Create(_playlistDefaultPath)) ;
+            }
+        }
+
+        private void LoadListToListbox()
+        {
+            string[] lines = File.ReadAllLines(_playlistDefaultPath);
+
+            foreach(var file in lines)
+            {
+                if (!string.IsNullOrWhiteSpace(file))
+                {
+                    mp3List.Add(file.Trim());
+                }
+            }
+
+            DisplayOnlyNames() ;
+            
+
+        }
+
+        private void SaveToTxt()
+        {
+            CreatePlaylistFile();
+            // Write the mp3List to the file
+            using (StreamWriter writer = new StreamWriter(_playlistDefaultPath))
+            {
+                foreach (string mp3File in mp3List)
+                {
+                    writer.WriteLine(mp3File);  // Write each mp3 file path to the file
+                }
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            SaveToTxt();
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+
+            // Check if any item is selected in listBox1
+            if (listBox1.SelectedItems.Count > 0)
+            {
+                // Create a list to hold the items to remove
+                List<string> itemsToRemove = new List<string>();
+
+                // Collect the selected items from listBox1
+                foreach (var selectedItem in listBox1.SelectedItems)
+                {
+                    string selectedItemName = selectedItem.ToString();
+                    // Find the corresponding item in mp3List
+                    string itemToRemove = mp3List.FirstOrDefault(mp3 => Path.GetFileName(mp3).Equals(selectedItemName, StringComparison.OrdinalIgnoreCase));
+                    if (itemToRemove != null)
+                    {
+                        itemsToRemove.Add(itemToRemove);
+                    }
+                }
+
+                // Remove each item from mp3List
+                foreach (var item in itemsToRemove)
+                {
+                    mp3List.Remove(item);
+                }
+
+                // Refresh the binding to update listBox1
+                DisplayOnlyNames();
+            }
+
+
+            else
+            {
+                // Show a message if no item is selected
+                MessageBox.Show("Please select an item to find and remove.", "No Selection", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadListToListbox();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            SaveToTxt();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // Get the search term from textBox1
+            string searchTerm = textBox1.Text.Trim();
+
+            // Create a filtered list based on the search term
+            if (string.IsNullOrEmpty(searchTerm))
+            {
+                // If the search term is empty, set the original list as the DataSource
+                _bindingSource.DataSource = mp3List.Select(mp3 => Path.GetFileName(mp3)).ToList();
+            }
+            else
+            {
+                // Filter the mp3List based on the search term
+                var filteredList = mp3List
+                    .Where(mp3 => Path.GetFileName(mp3).IndexOf(searchTerm, StringComparison.OrdinalIgnoreCase) >= 0)
+                    .Select(mp3 => Path.GetFileName(mp3))
+                    .ToList();
+
+                // Set the filtered list as the DataSource
+                _bindingSource.DataSource = filteredList;
+            }
+            _bindingSource.ResetBindings(false);
+        }
+    }
+}
